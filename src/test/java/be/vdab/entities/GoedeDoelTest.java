525e6e9a40e88387fdb2e068e90bb93e6bd8c8e6
Lisa Steendam <lisa.m.m.steendam@gmail.com>
2014-11-18 16:21:41 +0100

    Update test coverage: equals other class

--- src/test/java/be/vdab/entities/GoedeDoelTest.java
+++ src/test/java/be/vdab/entities/GoedeDoelTest.java
@@ -3,36 +3,44 @@ package be.vdab.entities;
 import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertNotEquals;
 
 public class GoedeDoelTest {
-	private static final String NAAM ="CliniClowns"; 
+	private static final String NAAM = "CliniClowns";
 	private GoedeDoel doel;
 
 	@Before
 	public void before() {
 		doel = new GoedeDoel(NAAM);
 	}
+
 	@Test
 	public void getNaam() {
 		assertEquals(NAAM, doel.getNaam());
 	}
-	
+
 	@Test
 	public void eenNieuwDoelHeeftNogNietsOpgebracht() {
 		assertEquals(0, doel.getOpgebracht().compareTo(BigDecimal.ZERO));
 	}
-	
+
 	@Test
 	public void goedeDoelenMetDezelfdeNaamMoetenVolgensEqualsGelijkZijn() {
 		assertEquals(new GoedeDoel("CLINICLOWNS"), doel);
 	}
-	
+
 	@Test
 	public void goedeDoelenMetDezelfdeNaamMoetenVolgensEqualsVerschillendZijn() {
 		assertNotEquals(new GoedeDoel("Unicef"), doel);
+	}
+
+	@Test
+	public void goedDoelVerschiltVanEenObjectMetEenAnderType() {
+		assertNotEquals(doel, new Date());
 	}
 }
