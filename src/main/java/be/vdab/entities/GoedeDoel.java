17b2fbe6450f3555243b7858ba03409add4ba6e1
Lisa Steendam <lisa.m.m.steendam@gmail.com>
2014-11-18 16:09:31 +0100

    Added: cobertura en override equals

--- src/main/java/be/vdab/entities/GoedeDoel.java
+++ src/main/java/be/vdab/entities/GoedeDoel.java
@@ -15,4 +15,12 @@ public class GoedeDoel {
 	public BigDecimal getOpgebracht() {
 		return opgebracht;
 	}
+	
+	@Override
+	public boolean equals(Object object) {
+		if (!(object instanceof GoedeDoel)) {
+			return false;
+		}
+		return ((GoedeDoel) object).naam.equalsIgnoreCase(naam);
+	}
 }
